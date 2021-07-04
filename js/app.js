0db3c0530001421462483263b31a0a03338d2469
Byoungkwon (Kwon) An <drancom@gmail.com>
2021-07-04 21:51:36 +0900

    Simplify the nav script

--- js/app.js
+++ js/app.js
@@ -10,22 +10,11 @@ $(document).ready(function(){
     });
 
     // Collapse toggle menu after click 
-
-    $(".nav-link-mobile").each(function () {
-        
-        $(this).on("click", function() {
-            
-            $(".hamburger-nav").click();
-        })
-    })
-
     $(document).click(function (event) {
         var clickover = $(event.target);
         var _opened = ($(".menu").css("display") == "block");
         if (_opened === true 
-            && !clickover.hasClass("hamburger-nav") 
-            && !clickover.hasClass("nav-link-mobile")
-            && !clickover.hasClass("nav-item-mobile")) {
+            && !clickover.hasClass("hamburger-nav")) {
             $(".hamburger-nav").click();
         }
     });
